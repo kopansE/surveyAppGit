44b1e694b05eb2a779ec2c4688f042478c782e1f
kopansE <evyatar.kopans@gmail.com>
2024-11-04 16:59:36 +0200

    allow user to choose profile to login with google - currently not doing anything with it

--- index.js
+++ index.js
@@ -1,8 +1,29 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
+const config = require('./config/keys');
+const keys = require('./config/keys');
+
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send({hi: 'there'});
-});
+// new GoogleStrategy will be an object named 'google' for later use.
+passport.use(new GoogleStrategy({ 
+    clientID: keys.googleClientID,
+    clientSecret: keys.googleClientSecret,
+    callbackURL: '/auth/google/callback'
+}, (acessToken) => {
+    console.log(acessToken);
+}));
 
-app.listen(5000);
+app.get('/auth/google',
+    passport.authenticate('google', {
+    scope: ['profile','email']
+})
+);
+
+// The app.gep below is for users who arent signing for the first time? 
+// The big diff is that it uses the callback function to print the access token 
+app.get('/auth/google/callback', passport.authenticate('google'));
+const PORT = process.env.PORT || 5000
+app.listen(PORT);
